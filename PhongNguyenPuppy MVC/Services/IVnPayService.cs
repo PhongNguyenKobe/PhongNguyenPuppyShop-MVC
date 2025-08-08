e5a33f5ddf123d77c1cd495a4d42f0cc16da56ee
YoshisukuArima <nguyentranankhang10d@gmail.com>
2025-08-08 08:22:48 +0700

    Thêm interface dịch vụ VnPay để tạo URL thanh toán và xử lý phản hồi

---
+++ PhongNguyenPuppy MVC/Services/IVnPayService.cs
@@ -0,0 +1,9 @@
+﻿using PhongNguyenPuppy_MVC.ViewModels;
+namespace PhongNguyenPuppy_MVC.Services
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(HttpContext context, VnPaymentRequestModel model);
+        VnPaymentResponseModel PaymentExecute(IQueryCollection collections);
+    }
+}
